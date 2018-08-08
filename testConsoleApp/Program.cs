1d134537426960db46dc912abf52b10f667764f9
antalmate12 <30597152+antalmate12@users.noreply.github.com>
2018-08-08 14:33:36 +0200

    firstCommit

--- testConsoleApp/Program.cs
+++ testConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,14 @@ namespace testConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+        }
+
+        void f1()
+        {
+            //var reader = new StreamReader;
+
         }
     }
 }
